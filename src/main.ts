f9859c40d87ee5fc9b67a0ae7681943595e51c29
Imran Ahmed <imran.ahk26@gmail.com>
2025-05-21 16:53:48 +0600

    Add Swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 🔧 Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('User API')
+    .setDescription('API for managing users')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Swagger UI will be available at /api
+
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
